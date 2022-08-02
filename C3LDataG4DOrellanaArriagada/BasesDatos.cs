e4111874f2c183dc004618d0fb298a977321c0cb
Marco Felipe Orellana <110493273+MarcoFelipeO@users.noreply.github.com>
2022-08-02 18:35:12 -0400

    Add files via upload

---
+++ C3LDataG4DOrellanaArriagada/BasesDatos.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using C3LNegG4DWOrellanaArriagada;
+using C3LDataG4DOrellanaArriagada;
+
+
+namespace C3LDataG4DOrellanaArriagada
+{
+    public class BasesDatos
+    {
+        
+
+
+       string conn = ConfigurationManager.AppSettings["strConexion"];
+
+        public Producto Ingresar(Producto objProducto)
+        {
+            //bool esExito = false;
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+                
+                cmd.Connection = new SqlConnection(conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "pa_insertar";
+                cmd.Parameters.Add("@codigo", SqlDbType.Int).Value = objProducto.codigo;
+                cmd.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = objProducto.nombre;
+                cmd.Parameters.Add("@proveedor", SqlDbType.VarChar, 50).Value = objProducto.proveedor;
+                cmd.Parameters.Add("@fechaadquisicion", SqlDbType.DateTime, 50).Value = objProducto.fechaadquisicion;
+                cmd.Parameters.Add("@cantidadadquirida", SqlDbType.Int).Value = objProducto.cantidadadquirida;
+                cmd.Parameters.Add("@stock", SqlDbType.Int).Value = objProducto.stock;
+                cmd.Parameters.Add("@nfactura", SqlDbType.Int).Value = objProducto.nfactura;
+                cmd.Parameters.Add("@fechafactura", SqlDbType.DateTime).Value = objProducto.fechafactura;
+
+
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                objProducto.esExito = true;
+
+            }
+            catch (SqlException ex)
+            {
+                objProducto.mensaje = "Excepcion capturada: " + ex;
+                objProducto.esExito = false;
+            }
+            return objProducto;
+        }// Fin ingresar
+
+        public Producto mostrar(Producto objProducto)
+        {
+           
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            try
+            {
+               cmd.Connection = new SqlConnection(conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "pa_mostrar";
+
+
+                cmd.Parameters.Add("@codigo", SqlDbType.Int).Value = objProducto.codigo;
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                sda.Fill(objProducto.ds, "tblProducto");
+                cmd.Connection.Close();
+
+            }
+            catch (SqlException ex)
+            {
+                objProducto.mensaje = "Excepcion capturada: " + ex;
+            }
+            return objProducto;
+        }// fin mostrar
+
+        public Producto eliminar(Producto objProducto)
+        {
+           
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            try
+            {
+
+               cmd.Connection = new SqlConnection(conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "pa_eliminar";
+                cmd.Parameters.Add("@codigo", SqlDbType.Int).Value = objProducto.codigo;
+
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                sda.Fill(objProducto.ds, "tblProducto");
+                cmd.Connection.Close();
+                objProducto.esExito = true;
+
+            }
+            catch (SqlException ex)
+            {
+                objProducto.mensaje = "Excepcion capturada: " + ex;
+                objProducto.esExito = false;
+            }
+
+            return objProducto;
+        }
+
+        public Producto modificar(Producto objProducto)
+        {
+            
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+               
+               cmd.Connection = new SqlConnection(conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "pa_modificar";
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = objProducto.id;
+                cmd.Parameters.Add("@codigo", SqlDbType.Int).Value = objProducto.codigo;
+                cmd.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = objProducto.nombre;
+                cmd.Parameters.Add("@proveedor", SqlDbType.VarChar, 50).Value = objProducto.proveedor;
+                cmd.Parameters.Add("@fechaadquisicion", SqlDbType.DateTime, 50).Value = objProducto.fechaadquisicion;
+                cmd.Parameters.Add("@cantidadadquirida", SqlDbType.Int).Value = objProducto.cantidadadquirida;
+                cmd.Parameters.Add("@stock", SqlDbType.Int).Value = objProducto.stock;
+                cmd.Parameters.Add("@nfactura", SqlDbType.Int).Value = objProducto.nfactura;
+                cmd.Parameters.Add("@fechafactura", SqlDbType.DateTime).Value = objProducto.fechafactura;
+
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                objProducto.esExito = true;
+
+            }
+            catch (SqlException ex)
+            {
+                objProducto.mensaje = "Excepcion capturada: " + ex;
+                objProducto.esExito = false;
+            }
+            return objProducto;
+        }
+    }
+}
+
---
+++ C3LNegG4DWOrellanaArriagada/Producto.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using C3LDataG4DOrellanaArriagada;
+
+
+
+namespace C3LNegG4DWOrellanaArriagada
+{
+    public class Producto
+    {
+        int _id;
+        int _codigo;
+        string _nombre;
+        string _proveedor;
+        DateTime _fechaadquisicion;
+        int _cantidadadquirida;
+        int _stock;
+        int _nfactura;
+        DateTime _fechafactura;
+        string _mensaje;
+        bool _esExito;
+        DataSet _ds = new DataSet();
+
+        public int id { get => _id; set => _id = value; }
+        public int codigo { get => _codigo; set => _codigo = value; }
+        public string nombre { get => _nombre; set => _nombre = value; }
+        public string proveedor { get => _proveedor; set => _proveedor = value; }
+        public DateTime fechaadquisicion { get => _fechaadquisicion; set => _fechaadquisicion = value; }
+        public int cantidadadquirida { get => _cantidadadquirida; set => _cantidadadquirida = value; }
+        public int stock { get => _stock; set => _stock = value; }
+        public int nfactura { get => _nfactura; set => _nfactura = value; }
+        public DateTime fechafactura { get => _fechafactura; set => _fechafactura = value; }
+        public string mensaje { get => _mensaje; set => _mensaje = value; }
+        public bool esExito { get => _esExito; set => _esExito = value; }
+
+        public DataSet ds { get => _ds; set => _ds = value; }
+
+        public Producto ingresar(Producto objProducto)
+        {
+            BasesDatos objDB = new BasesDatos();
+            objProducto = objDB.Ingresar(objProducto);
+            return objProducto;
+        }
+        public Producto mostrar(Producto objProducto)
+        {
+            BasesDatos objDB = new BasesDatos();
+            objProducto = objDB.mostrar(objProducto);
+            return objProducto;
+        }
+        public Producto eliminar(Producto objProducto)
+        {
+            BasesDatos objDB = new BasesDatos();
+            objProducto = objDB.eliminar(objProducto);
+            return objProducto;
+        }
+        public Producto modificar(Producto objProducto)
+        {
+            BasesDatos objDB = new BasesDatos();
+            objProducto = objDB.modificar(objProducto);
+            return objProducto;
+        }
+    }
+}
